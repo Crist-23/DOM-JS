6fe4767d2b1d6358e81ea0c60d5a60df7a7d074d
Crist-23 <cm654543@gmail.com>
2022-11-20 16:39:01 -0500

    changes

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-(() => {
-  const btn = document.querySelector("[data-form-btn]");
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteBtn.js";
 
-  //lee de arriba hacia abajo, tiene orden
-  const createTask =
-    ("click",
-    (event) => {
-      event.preventDefault();
-      const input = document.querySelector("[data-form-input]");
-      const value = input.value;
-      const list = document.querySelector("[data-list]");
-      const task = document.createElement("li");
-      task.classList.add("card");
-      input.value = "";
-      const taskContent = document.createElement("div");
-      const titleTask = document.createElement("span");
-      titleTask.classList.add("task");
-      titleTask.innerText = value;
-      taskContent.appendChild(checkComplete());
-      taskContent.appendChild(titleTask);
+const btn = document.querySelector("[data-form-btn]");
 
-      const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`; //backticks | Templ. strings decirle que value es una variable de JS
-      // task.innerHTML = content;
-      // el padre hace la lista y el hijo hace la tarea
-      task.appendChild(taskContent);
-      list.appendChild(task);
-    });
+const createTask = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+  //backticks
+  const taskContent = document.createElement("div");
 
-  // Arrow funtions o funciones anonimas
-  btn.addEventListener("click", createTask);
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+  // task.innerHTML = content;
 
-  const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask);
-    return i;
-  };
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
+  list.appendChild(task);
+};
 
-  const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("far");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-  };
-})();
+//Arrow functions o funciones anonimas
+btn.addEventListener("click", createTask);
