e184ced91ac14c27746be0c4962624b502bbb526
Crist-23 <110477298+Crist-23@users.noreply.github.com>
2022-11-21 16:14:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Este archivo lleva la logica
-
 import { addTask } from "./components/addTask.js";
 import { displayTask } from "./components/readTaks.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
-//Arrow functions o funciones anonimas
 btn.addEventListener("click", addTask);
 
 displayTask();
